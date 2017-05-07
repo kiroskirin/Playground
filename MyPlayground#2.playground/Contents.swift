863669a560c8c148e111edc37fd5756d6f30be23
Graisorn Soisakhoo <laa.kiroskirin@gmail.com>
2017-05-07 20:09:47 +0700

    update code

--- MyPlayground#2.playground/Contents.swift
+++ MyPlayground#2.playground/Contents.swift
@@ -3,35 +3,44 @@
 import UIKit
 
 func swapPosition(setCount: Int, operationCount: Int) -> String {
-    var loop = 0
+    
     var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8 ]
     let numberCount = numbers.count
     
-    while loop < setCount {
-        var op = 0
+    // loop set count
+    var set = 0
+    while set < setCount {
+        
         var x = 0
         var n = 0
-        while op < operationCount {
+        // loop operation count
+        var operation = 0
+        while operation < operationCount {
             
-            let a = numbers[x]
+            let a = numbers[ x ]
             n = x + 1
+            // check if index is equal to operation or array count
+            // then reset to zero
             if n == numberCount || n == operationCount {
                 n = 0
             }
             
-            let b = numbers[n]
+            let b = numbers[ n ]
             
-            numbers[x] = b
-            numbers[n] = a
+            numbers[ x ] = b
+            numbers[ n ] = a
             
-            op += 1
             x += 1
+            // check if index is equal to operation or array count
+            // then reset to zero
             if x == numberCount || x == operationCount {
                 x = 0
             }
+            
+            operation += 1
         }
         
-        loop += 1
+        set += 1
     }
     
     return numbers.map { "\($0)" }.joined(separator: " ").appending("\n")
